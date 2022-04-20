793db502ec903dabb8d7c196cd74234abb8011e4
Dell <okthichiatay77@gmail.com>
2022-04-20 20:29:24 +0700

    this is demo

--- ProfileSocial/settings.py
+++ ProfileSocial/settings.py
@@ -174,7 +174,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'okthichiatay77@gmail.com'
-EMAIL_HOST_PASSWORD = '0398397949'
+EMAIL_HOST_PASSWORD = '*********'
 
 
 LOGIN_URL = 'accounts:login'
